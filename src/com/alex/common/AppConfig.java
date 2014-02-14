cd1539ec6f2cf443a26d006fec00dffc04bca9fb
caisenchuan <caisenchuan@163.com>
2014-02-14 11:31:11 +0800

    关闭debug开关；

--- src/com/alex/common/AppConfig.java
+++ src/com/alex/common/AppConfig.java
@@ -9,7 +9,7 @@ public class AppConfig {
      * 常量
      *-------------------------*/
     /**全局调试开关*/
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
     
     ///////////////////////////////百度相关///////////////////////////////////
     /**百度 API KEY*/
@@ -19,7 +19,7 @@ public class AppConfig {
     /**应用的key 请到官方申请正式的appkey替换APP_KEY*/
     public static final String WEIBO_APP_KEY="3708502202";
     /**替换为开发者REDIRECT_URL*/
-    public static final String WEIBO_REDIRECT_URL = "http://caisenchuan.com/weibo/callback.php";
+    public static final String WEIBO_REDIRECT_URL = "http://caisenchuan.com/funweibo/callback.php";
     /**新支持scope 支持传入多个scope权限，用逗号分隔*/
     public static final String WEIBO_SCOPE = "email,direct_messages_read," +
     		                                 "direct_messages_write,friendships_groups_read," +
